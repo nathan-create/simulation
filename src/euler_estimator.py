b327ac136277bba0e2094b10bbf8b4a2d3eae483
NathanReynoso <sourlemonz111@gmail.com>
2021-02-16 00:02:05 +0000

    Added Hodgkin Huxley model and fixed issues in euler estimator

--- src/euler_estimator.py
+++ src/euler_estimator.py
@@ -5,28 +5,31 @@ class EulerEstimator:
     def __init__(self, derivatives):
         self.functions = derivatives
     
-    def calc_derivative_at_point(self, initial_point):
+    def calc_derivative_at_point(self, point):
         result_dict = {}
         for key in self.functions:
-            result_dict[key] = self.functions[key](initial_point[0],initial_point[1])
-            
+            result_dict[key] = self.functions[key](point[0],point[1])
         return result_dict
 
     def step_forward(self, point, step_size):
-        new_dict = {}
-        old_point = point[0]
-        derivs = self.calc_derivative_at_point(point)
-        for key in point[1]:
-            new_dict[key] =  round(point[1][key]+step_size*derivs[key],2)
-        return (old_point + step_size, new_dict)
+        t = point[0]
+        old_x = point[1]
+        deriv = self.calc_derivative_at_point(point)
+        new_x = {}
+        for key in old_x:
+            new_x[key] = old_x[key] + (deriv[key] * step_size)
+        new_point = (t + step_size, new_x)
+        return new_point
 
-    def calc_estimated_points(self, point, step_size, num_steps):
-        points_list = [point]
-        for num in range(num_steps):
-            new_point = self.step_forward(point,step_size)
-            points_list.append(new_point)
-            point = new_point
-        return points_list
+    def calc_estimated_points(self, point, step_size, max_val):
+        point_list = [point]
+        count = 0
+        while count < max_val:
+            next_point = self.step_forward(point, step_size)
+            point_list.append(next_point)
+            point = next_point
+            count += step_size
+        return point_list
 
     def plot(self, point, step_size, end_value):
         x_vals=[]
